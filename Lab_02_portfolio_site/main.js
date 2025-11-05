4bd905dcc5a48b6f5e711dfce7088bea42dd4358
pawel2004 <pawelgr7@gmail.com>
2025-11-05 15:21:42 +0100

    Forward with themes

--- Lab_02_portfolio_site/main.js
+++ Lab_02_portfolio_site/main.js
@@ -1,4 +1,5 @@
 const navigationButton = document.getElementById('open-menu');
+const changeThemeButton = document.getElementById('change-theme')
 const navigation = document.getElementById('nav-menu');
 const navigationLinks = document.querySelectorAll('.nav-link');
 
@@ -10,13 +11,19 @@ const toggleTheme = () => {
     if (document.body.classList.contains('light')) {
         document.body.classList.remove('light');
         document.body.classList.add('dark');
+        navigationButton.style.backgroundImage = 'url("assets/menu_icon_dark.svg")';
+        changeThemeButton.style.backgroundImage = 'url("assets/dark_mode.svg")';
     } else {
         document.body.classList.remove('dark');
         document.body.classList.add('light');
+        navigationButton.style.backgroundImage = 'url("assets/menu_icon_light.svg")';
+        changeThemeButton.style.backgroundImage = 'url("assets/light_mode.svg")';
     }
 }
 
 navigationButton.addEventListener('click', toggleNavbar);
+
+changeThemeButton.addEventListener('click', toggleTheme);
 
 navigationLinks.forEach((e) => {
     e.addEventListener('click', toggleNavbar);
